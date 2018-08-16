915986d65cdfce112efa6a77c7f71f450efa92b7
Carlos Grijalva <grijalva.carlos@gmail.com>
2018-08-16 11:36:00 -0400

    Retrieve method name from request

--- index.js
+++ index.js
@@ -4,10 +4,25 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
-// The servershould respond to all request with a string
+// The server should respond to all request with a string
 const server = http.createServer((req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the HTTP Method
+  const method = req.method.toLowerCase();
+
+  // Send the response
   res.end('Hello World\n');
+
+  // Log the request path
+  console.log(`Request receieved on path : ${trimmedPath} with method: ${method}`);
 });
 
 // Start the server, and have it listen to port 3000
